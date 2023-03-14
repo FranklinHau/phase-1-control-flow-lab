bb0ed28634b16cde23a734430905c5d51fdbe58c
Franklin Hau <fhauchi@gmail.com>
2023-03-14 02:57:43 -0600

    control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(perFeet){
+  let cost;
+  if (perFeet <= 400){
+    cost = 'This one is on me!';
+  }else if (perFeet <= 2000){ 
+    cost = 'That will be twenty bucks.';
+  }else if (perFeet <= 2500){
+    cost = 'I will gladly take your thirty bucks.';
+  }else {
+    cost = 'No can do.';
+  }
+  return cost
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const destination = city === 'NYC'? 'Ok, sounds good.' : 'No go.';
+  return destination;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(string){
+  let response;
+  switch(string){
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = "Bye."
+      break; 
+  } return response
 }
